d36b4110d01b990cd8ea3fe6bae1f86603e91afa
Lari Rauno <tuutti@iki.fi>
2015-08-24 12:34:23 +0300

    initial stuff

---
+++ src/Provider/Office365.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * @file
+ * Contains Tuutti\Oauth2\Client\Provider\Office365.
+ */
+
+namespace Tuutti\OAuth2\Client\Provider;
+
+use League\OAuth2\Client\Provider\AbstractProvider;
+use League\OAuth2\Client\Provider\Exception\IdentityProviderException;
+use League\OAuth2\Client\Token\AccessToken;
+use Psr\Http\Message\ResponseInterface;
+
+class Office365 Extends AbstractProvider {
+}
